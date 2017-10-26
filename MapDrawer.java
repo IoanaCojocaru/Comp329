a8778eeb6a8f547379fd8be21c9315f22fac1a8b
Gab13 <gabriela.r1303@gmail.com>
2017-10-26 15:17:50 +0100

    Add files via upload
    Simple grid drawer

---
+++ MapDrawer.java
@@ -0,0 +1,61 @@
+import lejos.hardware.Brick;
+import lejos.hardware.BrickFinder;
+import lejos.hardware.lcd.GraphicsLCD;
+
+
+public class MapDrawer  {
+	private static Brick myEV3 = BrickFinder.getDefault();
+	private static GraphicsLCD lcd = myEV3.getGraphicsLCD();
+	int robotLength = 25;
+	int robotWidth = 20;
+	int r;
+	int t;
+	
+	public void initMap(int [][]map) {
+		r = 0;
+		t = 0;
+
+		for (int i = 0; i < map.length; i++) {
+			r = 0;
+			for (int j = 0; j < map[0].length; j++) {
+				
+				lcd.drawRect(j+r, i + t, robotLength, robotWidth);
+				
+				r = r + 25;	//Set space between the rows		
+			}	
+			t = t + 20; //Set space between the columns
+		}
+	}
+	
+	//For an individual cell
+	public void updateMap(int [][]map, int i,int j) {
+		r = 0;
+		t = 0;
+		
+		if (map[i][j] != 0)
+			lcd.fillRect(j+r, i + t, robotLength, 20);
+		else
+			lcd.drawRect(j+r, i + t, robotLength, 20);
+	}	
+	
+	public void printMap(int [][]map) {
+		r = 0;
+		t = 0;
+
+		for (int i = 0; i < map.length; i++) {
+			r = 0;
+			for (int j = 0; j < map[0].length; j++) {
+				
+				if (map[i][j] != 0)
+					lcd.fillRect(j+r, i + t, robotLength, robotWidth);
+				else
+					lcd.drawRect(j+r, i + t,robotLength, robotWidth);
+				
+				r = r + 25;			
+			}	
+			t = t + 20;
+		}
+	}
+}
+
+
